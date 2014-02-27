bc71c098b0727403e45657bf203f7414a00091ef
caytekin <cigdemaytekin_872@hotmail.com>
2014-02-27 16:36:58 +0100

    commit for added files

---
+++ InheritanceSamples/src/edu/uva/analysis/samples/InterfaceRunner.java
@@ -0,0 +1,22 @@
+package edu.uva.analysis.samples;
+
+
+
+
+
+public abstract class InterfaceRunner {
+	abstract Object getObject() throws Exception;
+
+	final Object getApiObject() throws Exception{
+		Object obj = getObject();
+		if(obj instanceof MyMutable){
+			return ((MyMutable)obj).getImmutableObject();
+		}
+		return obj;
+	}
+	
+	void tryWithAPackage() {
+		edu.uva.analysis.gensamples.Rectangle myRectangle = new edu.uva.analysis.gensamples.Rectangle();
+	}
+
+}
---
+++ InheritanceSamples/src/edu/uva/analysis/samples/MyMutable.java
@@ -0,0 +1,7 @@
+package edu.uva.analysis.samples;
+
+public interface MyMutable {
+	
+	Object getImmutableObject();
+
+}
