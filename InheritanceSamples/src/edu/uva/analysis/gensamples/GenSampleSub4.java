79eff2d251885b7f0a8de87d7e6ca8bae4394573
caytekin <cigdemaytekin_872@hotmail.com>
2014-03-11 16:52:59 +0100

    added files

---
+++ InheritanceSamples/src/edu/uva/analysis/gensamples/GenSampleSub4.java
@@ -0,0 +1,5 @@
+package edu.uva.analysis.gensamples;
+
+public class GenSampleSub4  <T extends Cloneable & MyRotatable> extends GenSample4 {
+
+}
---
+++ InheritanceSamples/src/edu/uva/analysis/gensamples/MyCloneAndRotatable.java
@@ -0,0 +1,5 @@
+package edu.uva.analysis.gensamples;
+
+public class MyCloneAndRotatable implements Cloneable, MyRotatable {
+
+}
